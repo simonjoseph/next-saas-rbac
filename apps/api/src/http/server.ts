8e4b7f75764fb201c5f35bf82cc63eb65edb270f
simonjoseph <simaojose1978@gmail.com>
2024-09-18 19:37:50 +0100

    init the api

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,19 @@
+import { fastify } from 'fastify'
+import fastifyCors from '@fastify/cors'
+import {
+  // jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log(`🚀 Server ready at http://localhost:3000`)
+})
---
+++ apps/api/src/index.ts
@@ -0,0 +1,8 @@
+import { defineAbilityFor, projectSchema } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
+
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
+
+console.log(ability.can('delete', project))
+console.log(ability.can('get', 'Billing'))
